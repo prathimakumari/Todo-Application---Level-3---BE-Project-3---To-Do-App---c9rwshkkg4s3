c03ed871f57dfdaface708e1bc23814b9209b6cb
prathimakumari <115368988+prathimakumari@users.noreply.github.com>
2023-05-30 15:31:06 +0530

    Update taskControllers.js

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,68 +1,62 @@
-const Users   = require("../models/user.js");
+const Users = require("../models/user.js");
 const jwt = require("jsonwebtoken");
-const Tasks   = require("../models/task.js");
-const bcrypt  = require('bcrypt');
+const Tasks = require("../models/task.js");
+const bcrypt = require("bcrypt");
 const { valid } = require("joi");
 const JWT_SECRET = "newtonSchool";
 
+const createTask = async (req, res) => {
+  //creator_id is user id who have created this task.
 
-const createTask =async (req, res) => {
+  const { heading, description, token } = req.body;
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid token",
+    });
+  }
+  const creator_id = decodedToken.userId;
 
-    //creator_id is user id who have created this task.
+  const newtask = {
+    heading,
+    description,
+    creator_id,
+  };
 
-    const { heading, description, token  } = req.body;
-    let decodedToken;
-    try{
-        decodedToken = jwt.verify(token, JWT_SECRET);
-    }catch(err){
-        res.status(404).json({
-            "status": 'fail',
-            "message": 'Invalid token'
-        });
-    }
-    const creator_id = decodedToken.userId;
-
-    const newtask = {
-        heading,
-        description,
-        creator_id
-    };
-
-    try{
-        const task = await Tasks.create(newtask);
-        res.status(200).json({
-            message: 'Task added successfully',
-            task_id: task._id,
-            status: 'success'
-        });
-    }catch(error){
-        res.status(404).json({
-            status: 'fail',
-            message: error.message
-        });
-    };
-
-}
-
+  try {
+    const task = await Tasks.create(newtask);
+    res.status(200).json({
+      message: "Task added successfully",
+      task_id: task._id,
+      status: "success",
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
 
 const getdetailTask = async (req, res) => {
+  const task_id = req.body.task_id;
 
-    const task_id = req.body.task_id;
-
-    try{
-        const task = await Tasks.findById(task_id);
-        res.status(200).json({
-            status: 'success',
-            data: task
-        })
-    }catch(err){
-        res.status(404).json({
-            status: 'fail',
-            message: err.message
-        })
-    }
-}
-
+  try {
+    const task = await Tasks.findById(task_id);
+    res.status(200).json({
+      status: "success",
+      data: task,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
 /*
 
@@ -109,13 +103,24 @@ json = {
 
 */
 
-
 const updateTask = async (req, res) => {
-    
-    const task_id = req.body.task_id;
-    //Write your code here.
-}
-
+  const task_id = req.body.task_id;
+  //Write your code here.
+  try {
+    const updatedTask = await Tasks.findByIdAndUpdate(task_id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: updatedTask,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
 /*
 
@@ -151,11 +156,20 @@ json = {
 */
 
 const deleteTask = async (req, res) => {
-
-    const task_id = req.body.task_id;
-    //Write your code here.
-
-}
-
+  const task_id = req.body.task_id;
+  //Write your code here.
+  try {
+    const task = await Tasks.findByIdAndDelete(task_id);
+    res.status(200).json({
+      status: "success",
+      message: "Task deleted successfully",
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
 module.exports = { createTask, getdetailTask, updateTask, deleteTask };
